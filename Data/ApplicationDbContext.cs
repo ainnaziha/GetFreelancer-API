de62ec96114ec68993add02a5b8e27cc872a413d
ainnaziha <ainnaziha551@gmail.com>
2023-09-17 23:14:42 +0800

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using GetFreelancer_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetFreelancer_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Freelancer> Freelancers { get; set; }
+    }
+}
---
+++ Migrations/20230917115823_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GetFreelancer_API.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Freelancers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Skillset = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    hobby = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Freelancers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Freelancers");
+        }
+    }
+}
