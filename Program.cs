3c36cea32f10d49e19e69b5abbfcd46c7db5a8d3
ainnaziha <ainnaziha551@gmail.com>
2023-09-18 03:31:44 +0800

    fix: cors allow all

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
